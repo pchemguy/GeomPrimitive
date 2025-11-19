5f39ff4fa210d0f69f529eeb2af5f99efbcce4f3
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2025-11-19 17:38:43 +0300

    u

--- primitives/demo/demo1.py
+++
@@ -1,4 +0,0 @@
-from gshapes.core import create_circle
-
-circle = create_circle(5)
-print(f"Circle with radius {circle[0]} has area {circle[1]}")
--- primitives/demo/demo2.py
+++
@@ -1,4 +0,0 @@
-from gshapes.utils import some_utility_function
-
-if some_utility_function():
-    print("Utility function works!")
